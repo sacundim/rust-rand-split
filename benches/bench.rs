295420225ea373d4902a4f519ed8d3c7a7ee375f
Luis Casillas <luis@casillas.org>
2016-05-14 14:50:25 -0700

    Add a benchmark suite (mostly lifted from `rand` crate).

--- benches/bench.rs
+++ benches/bench.rs
@@ -8,7 +8,9 @@ const RAND_BENCH_N: u64 = 1000;
 
 use std::mem::size_of;
 use test::{black_box, Bencher};
-use rand::{Rng, OsRng};
+use rand::{Rng, OsRng, StdRng, XorShiftRng};
+use rand::chacha::ChaChaRng;
+use rand::isaac::{IsaacRng, Isaac64Rng};
 use split_rand::siprng::SipRng;
 
 #[bench]
@@ -21,3 +23,69 @@ fn rand_siprng(b: &mut Bencher) {
     });
     b.bytes = size_of::<usize>() as u64 * RAND_BENCH_N;
 }
+
+/*
+ * The following benches are lifted straight from the `rand` crate.
+ * Having them here is just convenient.
+ */
+
+#[bench]
+fn rand_xorshift(b: &mut Bencher) {
+    let mut rng: XorShiftRng = OsRng::new().unwrap().gen();
+    b.iter(|| {
+        for _ in 0..RAND_BENCH_N {
+            black_box(rng.gen::<usize>());
+        }
+    });
+    b.bytes = size_of::<usize>() as u64 * RAND_BENCH_N;
+}
+
+#[bench]
+fn rand_isaac(b: &mut Bencher) {
+    let mut rng: IsaacRng = OsRng::new().unwrap().gen();
+    b.iter(|| {
+        for _ in 0..RAND_BENCH_N {
+            black_box(rng.gen::<usize>());
+        }
+    });
+    b.bytes = size_of::<usize>() as u64 * RAND_BENCH_N;
+}
+
+#[bench]
+fn rand_isaac64(b: &mut Bencher) {
+    let mut rng: Isaac64Rng = OsRng::new().unwrap().gen();
+    b.iter(|| {
+        for _ in 0..RAND_BENCH_N {
+            black_box(rng.gen::<usize>());
+        }
+    });
+    b.bytes = size_of::<usize>() as u64 * RAND_BENCH_N;
+}
+
+#[bench]
+fn rand_std(b: &mut Bencher) {
+    let mut rng = StdRng::new().unwrap();
+    b.iter(|| {
+        for _ in 0..RAND_BENCH_N {
+            black_box(rng.gen::<usize>());
+        }
+    });
+    b.bytes = size_of::<usize>() as u64 * RAND_BENCH_N;
+}
+
+
+/*
+ * The `rand` crate is missing a benchmark for `ChaChaRng`.
+ */
+
+#[bench]
+fn rand_chacha(b: &mut Bencher) {
+    let mut rng: ChaChaRng = OsRng::new().unwrap().gen();
+    b.iter(|| {
+        for _ in 0..RAND_BENCH_N {
+            black_box(rng.gen::<usize>());
+        }
+    });
+    b.bytes = size_of::<usize>() as u64 * RAND_BENCH_N;
+}
+
