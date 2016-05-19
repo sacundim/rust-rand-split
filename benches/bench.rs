03d78709f03f01efe865e0c8a3f9bded84bf149b
Luis Casillas <luis.casillas@progressfin.com>
2016-05-19 10:17:39 -0700

    Refactor benchmarks to elimininate duplication, and add more PCG benches.

--- benches/bench.rs
+++ benches/bench.rs
@@ -9,8 +9,8 @@ extern crate test;
 const RAND_BENCH_N: u64 = 1000;
 
 use mersenne_twister::{MT19937, MT19937_64};
-use pcg::Pcg32;
-use rand::{Rng, SeedableRng, OsRng, StdRng, XorShiftRng};
+use pcg::{Pcg32, Pcg32Fast, Pcg32L, Pcg32LFast, Pcg64, Pcg64Fast};
+use rand::{Rng, SeedableRng, Rand, thread_rng, StdRng, XorShiftRng};
 use rand::chacha::ChaChaRng;
 use rand::isaac::{IsaacRng, Isaac64Rng};
 use rand_split::Split;
@@ -18,9 +18,12 @@ use rand_split::siprng::SipRng;
 use std::mem::size_of;
 use test::{black_box, Bencher};
 
-#[bench]
-fn rand_siprng(b: &mut Bencher) {
-    let mut rng: SipRng = OsRng::new().unwrap().gen();
+// Adapted from the `rand` crate.
+fn bench_random_rng<R: Rng + Rand>(b: &mut Bencher) {
+    bench_rng::<R>(b, &mut thread_rng().gen());
+}
+
+fn bench_rng<R: Rng>(b: &mut Bencher, rng: &mut R) {
     b.iter(|| {
         for _ in 0..RAND_BENCH_N {
             black_box(rng.gen::<usize>());
@@ -29,15 +32,15 @@ fn rand_siprng(b: &mut Bencher) {
     b.bytes = size_of::<usize>() as u64 * RAND_BENCH_N;
 }
 
+
 #[bench]
-fn rand_split_isaac64(b: &mut Bencher) {
-    let mut rng: Split<Isaac64Rng> = OsRng::new().unwrap().gen();
-    b.iter(|| {
-        for _ in 0..RAND_BENCH_N {
-            black_box(rng.gen::<usize>());
-        }
-    });
-    b.bytes = size_of::<usize>() as u64 * RAND_BENCH_N;
+fn rand_siprng(b: &mut Bencher) {
+    bench_random_rng::<SipRng>(b);
+}
+
+#[bench]
+fn rand_split_sip_isaac64(b: &mut Bencher) {
+    bench_random_rng::<Split<Isaac64Rng>>(b);
 }
 
 /*
@@ -47,46 +50,22 @@ fn rand_split_isaac64(b: &mut Bencher) {
 
 #[bench]
 fn rand_xorshift(b: &mut Bencher) {
-    let mut rng: XorShiftRng = OsRng::new().unwrap().gen();
-    b.iter(|| {
-        for _ in 0..RAND_BENCH_N {
-            black_box(rng.gen::<usize>());
-        }
-    });
-    b.bytes = size_of::<usize>() as u64 * RAND_BENCH_N;
+    bench_random_rng::<XorShiftRng>(b);
 }
 
 #[bench]
 fn rand_isaac(b: &mut Bencher) {
-    let mut rng: IsaacRng = OsRng::new().unwrap().gen();
-    b.iter(|| {
-        for _ in 0..RAND_BENCH_N {
-            black_box(rng.gen::<usize>());
-        }
-    });
-    b.bytes = size_of::<usize>() as u64 * RAND_BENCH_N;
+    bench_random_rng::<IsaacRng>(b);
 }
 
 #[bench]
 fn rand_isaac64(b: &mut Bencher) {
-    let mut rng: Isaac64Rng = OsRng::new().unwrap().gen();
-    b.iter(|| {
-        for _ in 0..RAND_BENCH_N {
-            black_box(rng.gen::<usize>());
-        }
-    });
-    b.bytes = size_of::<usize>() as u64 * RAND_BENCH_N;
+    bench_random_rng::<Isaac64Rng>(b);
 }
 
 #[bench]
 fn rand_std(b: &mut Bencher) {
-    let mut rng = StdRng::new().unwrap();
-    b.iter(|| {
-        for _ in 0..RAND_BENCH_N {
-            black_box(rng.gen::<usize>());
-        }
-    });
-    b.bytes = size_of::<usize>() as u64 * RAND_BENCH_N;
+    bench_rng::<StdRng>(b, &mut StdRng::new().unwrap());
 }
 
 
@@ -96,13 +75,7 @@ fn rand_std(b: &mut Bencher) {
 
 #[bench]
 fn rand_chacha(b: &mut Bencher) {
-    let mut rng: ChaChaRng = OsRng::new().unwrap().gen();
-    b.iter(|| {
-        for _ in 0..RAND_BENCH_N {
-            black_box(rng.gen::<usize>());
-        }
-    });
-    b.bytes = size_of::<usize>() as u64 * RAND_BENCH_N;
+    bench_random_rng::<ChaChaRng>(b);
 }
 
 
@@ -112,26 +85,14 @@ fn rand_chacha(b: &mut Bencher) {
 
 #[bench]
 fn rand_mt19937(b: &mut Bencher) {
-    let seed: u64 = OsRng::new().unwrap().gen();
-    let mut rng: MT19937 = SeedableRng::from_seed(seed);
-    b.iter(|| {
-        for _ in 0..RAND_BENCH_N {
-            black_box(rng.gen::<usize>());
-        }
-    });
-    b.bytes = size_of::<usize>() as u64 * RAND_BENCH_N;
+    let seed: u64 = thread_rng().gen();
+    bench_rng::<MT19937>(b, &mut SeedableRng::from_seed(seed));
 }
 
 #[bench]
 fn rand_mt19937_64(b: &mut Bencher) {
-    let seed: u64 = OsRng::new().unwrap().gen();
-    let mut rng: MT19937_64 = SeedableRng::from_seed(seed);
-    b.iter(|| {
-        for _ in 0..RAND_BENCH_N {
-            black_box(rng.gen::<usize>());
-        }
-    });
-    b.bytes = size_of::<usize>() as u64 * RAND_BENCH_N;
+    let seed: u64 = thread_rng().gen();
+    bench_rng::<MT19937_64>(b, &mut SeedableRng::from_seed(seed));
 }
 
 /*
@@ -140,12 +101,31 @@ fn rand_mt19937_64(b: &mut Bencher) {
 
 #[bench]
 fn rand_pgc32(b: &mut Bencher) {
-    let mut rng: Pcg32 = OsRng::new().unwrap().gen();
-    b.iter(|| {
-        for _ in 0..RAND_BENCH_N {
-            black_box(rng.gen::<usize>());
-        }
-    });
-    b.bytes = size_of::<usize>() as u64 * RAND_BENCH_N;
+    bench_random_rng::<Pcg32>(b);
+}
+
+#[bench]
+fn rand_pgc32_fast(b: &mut Bencher) {
+    bench_random_rng::<Pcg32Fast>(b);
+}
+
+#[bench]
+fn rand_pgc32l(b: &mut Bencher) {
+    bench_random_rng::<Pcg32L>(b);
+}
+
+#[bench]
+fn rand_pgc32l_fast(b: &mut Bencher) {
+    bench_random_rng::<Pcg32LFast>(b);
+}
+
+#[bench]
+fn rand_pgc64(b: &mut Bencher) {
+    bench_random_rng::<Pcg64>(b);
+}
+
+#[bench]
+fn rand_pgc64_fast(b: &mut Bencher) {
+    bench_random_rng::<Pcg64Fast>(b);
 }
 
